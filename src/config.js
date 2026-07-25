3a066b5777b22338e609d65c2e19d3efb67c2466
shaw29punam-developer <shaw.29punam@gmail.com>
2026-07-25 21:22:47 +0530

    chanegs by me

--- src/config.js
+++ src/config.js
@@ -2,8 +2,9 @@ const moongo = require("mongoose");
 
 const connectDatabase = async () => {
     const url = 'mongodb+srv://shaw29punam_db_user:WrsWzKiPwKVjrkjG@punam-node-cluster.1nvowha.mongodb.net/?appName=punam-node-cluster';
-
+ 
     await moongo.connect(url);
+    console.log("Database connected successfully");
 }
 
 
